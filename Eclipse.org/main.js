9b2c3be154f92fc7bbf99ee2826d9c992181c005
premgoyal09 <163977408+premgoyal09@users.noreply.github.com>
2024-08-05 22:28:41 +0530

    Add files via upload

---
+++ Eclipse.org/main.js
@@ -0,0 +1,56 @@
+$(document).ready(function () {
+    $("#hidet").click(function () {
+        $(".searchbtnjqy").toggle();
+    })
+})
+
+$(document).ready(function () {
+    $(".link1").click(function () {
+        $(".mainnav1").toggle();
+    })
+})
+
+
+$(document).ready(function () {
+    $(".link2").click(function () {
+        $(".mainnav2").toggle();
+    })
+
+})
+
+$(document).ready(function () {
+    $(".link3").click(function () {
+        $(".mainnav3").toggle();
+    })
+})
+
+$(document).ready(function () {
+    $(".link4").click(function () {
+        $(".mainnav4").toggle();
+    })
+})
+
+$(document).ready(function () {
+    $(".link5").click(function () {
+        $(".mainnav5").toggle();
+    })
+})
+
+$(document).ready(function () {
+    $(".link1").click(function () {
+        $(".nav-linkbtn").toggle();
+    })
+    $(".link2").click(function () {
+        $(".nav2-linkbtn2").toggle();
+    })
+    $(".link3").click(function () {
+        $(".nav3-linkbtn3").toggle();
+    })
+    $(".link4").click(function () {
+        $(".nav4-linkbtn4").toggle();
+    })
+    $(".link5").click(function () {
+        $(".nav5-linkbtn5").toggle();
+    })
+})
+
